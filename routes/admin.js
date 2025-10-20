1bb7a45c03e161d9ed5b82f3586b1ff095165847
Osama Ibrahim <osarezk119@gmail.com>
2025-10-21 01:51:32 +0300

    feat: Add views folder

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 
 router.get('/add-product', (req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="Submit">add');
+    res.sendFile(path.join(__dirname , '../', 'views', 'add-product.html'))
 });
 
 router.post('/product', (req, res, next) => {
