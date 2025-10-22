222200f35a2ad48c8233b21487d53db455517c2e
Osama Ibrahim <osarezk119@gmail.com>
2025-10-23 00:05:17 +0300

    refactor: Replace Handlebars with EJS as templating engine

--- routes/shop.js
+++ routes/shop.js
@@ -5,7 +5,7 @@ const rootDir = require('../util/path')
 const adminData = require('./admin')
 
 router.get('/', (req, res, next) => {
-    res.render('shop', {prods: adminData.products, docTitle: 'المتجر - جميع المنتجات'});
+    res.render('shop', {prods: adminData.products, pageTitle: 'المتجر - جميع المنتجات', path:  '/', hasProduct: adminData.products.length > 0});
 });
 
 module.exports = router
