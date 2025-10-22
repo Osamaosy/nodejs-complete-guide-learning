739be66c6ebd95b77c416f1be244c10482bc3076
Osama Ibrahim <osarezk119@gmail.com>
2025-10-22 03:14:32 +0300

    feat: Integrate Pug engine and render initial template

--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const rootDir = require('../util/path')
-
+const adminData = require('./admin')
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    res.render('shop');
 });
 
 module.exports = router
